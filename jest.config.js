230a2488bce0eb1e9e11f57e91de0ed7400c1fed
Donta <donta.ruffin@gmail.com>
2025-11-17 17:46:25 -0500

    Fix jest config merge conflict

--- jest.config.js
+++ jest.config.js
@@ -1,31 +1,3 @@
-main
-/** @type {import('jest').Config} */
-module.exports = {
-  testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  testMatch: [
-    '**/__tests__/**/*.[jt]s?(x)',
-    '**/?(*.)+(spec|test).[jt]s?(x)'
-  ],
-  collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/**/*.stories.{js,jsx,ts,tsx}',
-  ],
-  preset: 'ts-jest',
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: {
-        jsx: 'react',
-        esModuleInterop: true,
-      }
-    }],
-  },
-};
-=======
 const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
@@ -33,12 +5,20 @@ const createJestConfig = nextJest({
 })
 
 const customJestConfig = {
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    '^@/components/(.*)$': '<rootDir>/components/$1',
-  },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.stories.{js,jsx,ts,tsx}',
+  ],
 }
 
 module.exports = createJestConfig(customJestConfig)
-code-quality-improvements
